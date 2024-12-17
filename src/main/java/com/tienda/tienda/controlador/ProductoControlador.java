53e43712538d0f12e522f8474bb4d35ce4c7fd7a
EmiroMunoz01 <emiro_2001@icloud.com>
2024-12-17 15:25:13 -0500

    Se agregó el método de eliminar producto entonces se creó el método en el controlador en donde se le pasa como parámetro el id con @path variable y se llama al servicio para eliminar el producto determinado que también se le pasa como parámetro del id luego de esto vamos a ir a la vista de mostrar y vamos a agregar esta funcionalidad al botón de eliminar de forma similar como hicimos con el botón de editar

--- src/main/java/com/tienda/tienda/controlador/ProductoControlador.java
+++ src/main/java/com/tienda/tienda/controlador/ProductoControlador.java
@@ -6,6 +6,7 @@ import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,10 +63,14 @@ public class ProductoControlador {
 
     @PostMapping("/actualizar")
     public String actualizar(Producto producto) {
-
         productoServicio.actualizarProducto(producto);
-
         return "redirect:/productos";
     }
 
+    @GetMapping("/eliminar/{id}")
+    public String eliminarProducto(@PathVariable Integer id) {
+        productoServicio.eliminarProducto(id);
+        return "redirect:/productos";
+    }
+    
 }
