3c359283e3a7819f04c9d140748fcf415c95fec6
EmiroMunoz01 <emiro_2001@icloud.com>
2024-12-17 16:40:45 -0500

    se ha creado el servicio de cargar imagen con este servicio vamos a poder eliminar o guardar una imagen esto lo inyectaremos utilizando la inyección de dependencias en el controlador del producto

---
+++ src/main/java/com/tienda/tienda/servicio/CargarArchivoServicio.java
@@ -0,0 +1,30 @@
+package com.tienda.tienda.servicio;
+
+import java.io.*;
+import java.nio.file.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class CargarArchivoServicio {
+
+    private String carpeta = "imagen//";
+
+    public String guardarImagen(MultipartFile archivo) throws IOException {
+        if (!archivo.isEmpty()) {
+            byte[] bytes = archivo.getBytes();
+            Path path = Paths.get(carpeta + archivo.getOriginalFilename());
+            Files.write(path, bytes);
+            return archivo.getOriginalFilename();
+        }
+        return "default.jpg";
+    }
+
+    public void eliminarImagen(String nombre) {
+        String ruta = "image//";
+        File file = new File(ruta + nombre);
+        file.delete();
+    }
+
+}
