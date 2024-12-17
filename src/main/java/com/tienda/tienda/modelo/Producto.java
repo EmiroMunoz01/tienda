a70ec4b47b2ea3ca93a4a8d9795f700ab7eb368e
EmiroMunoz01 <emiro_2001@icloud.com>
2024-12-17 11:56:22 -0500

    Se ha tenido que modificar el método tu string debido a que la dependencia lombok no nos fue de mucha utilidad en esta ocasión debido a esto hemos recreado el método pero exceptuando el parámetro de usuario debido a que no existe este y genera un error al momento de imprimir por el logger la información que estamos recibiendo por consola

--- src/main/java/com/tienda/tienda/modelo/Producto.java
+++ src/main/java/com/tienda/tienda/modelo/Producto.java
@@ -32,6 +32,77 @@ public class Producto {
     private double precio;
     private int cantidad;
 
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", nombre='" + getNombre() + "'" +
+            ", descripcion='" + getDescripcion() + "'" +
+            ", imagen='" + getImagen() + "'" +
+            ", precio='" + getPrecio() + "'" +
+            ", cantidad='" + getCantidad() + "'"  ;
+    }
+
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return this.imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return this.cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Usuario getUsuario() {
+        return this.usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+
     // 2 relacionaremos este campo con la tabla de usuarios
 
     // 4 le diremos que este campo tiene una relacion con el campo usuario de muchos a uno, lo que hara internamente el framework es crear un campo en la tabla productos para enviar el id del usuario y que se mapee directamente en la clase en el atributo usuario que se ha declarado en esta clase
