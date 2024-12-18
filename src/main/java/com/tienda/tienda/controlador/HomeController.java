1bffa9fd05424ef3bc9a554eb76f38000b03c8b2
EmiroMunoz01 <emiro_2001@icloud.com>
2024-12-18 18:05:24 -0500

    Se ha configurado la vista del producto para agregarlo al carrito esto se ha logrado gracias a que se ha integrado el controlador en home controller y en la vista de home html pasando como parámetro su id al momento de crear el controlador del objeto

--- src/main/java/com/tienda/tienda/controlador/HomeController.java
+++ src/main/java/com/tienda/tienda/controlador/HomeController.java
@@ -1,16 +1,24 @@
 package com.tienda.tienda.controlador;
 
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tienda.tienda.servicio.ProductoServicio;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    // crearemos la variable logger para saber que sucede dentro de la variable
+
+    private final Logger log = org.slf4j.LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductoServicio productoServicio;
@@ -21,6 +29,14 @@ public class HomeController {
         return "usuario/home";
     }
 
+    // buscaremos el id del producto y lo enviaremos a otra vista con la informacion
+    // del producto
 
+    @GetMapping("productohome/{id}")
+    public String productoHome(
+            @PathVariable Integer id) {
+        log.info("Id enviado como parametro {}", id);
+        return "usuario/productohome";
+    }
 
 }
